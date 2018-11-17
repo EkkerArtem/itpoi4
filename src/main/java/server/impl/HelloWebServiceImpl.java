2cf7d24d08882fa77ac4e1b1d2b44c060f3a7365
Ekker_Artem <40545726+jetess@users.noreply.github.com>
2018-11-17 21:04:18 +0200

    Server part added.

---
+++ src/main/java/server/impl/HelloWebServiceImpl.java
@@ -0,0 +1,17 @@
+package server.impl;
+
+// таже аннотация, что и при описании интерфейса,
+import server.HelloWebService;
+
+import javax.jws.WebService;
+
+// но здесь используется с параметром endpointInterface,
+// указывающим полное имя класса интерфейса нашего веб-сервиса
+@WebService(endpointInterface = "server.HelloWebService")
+public class HelloWebServiceImpl implements HelloWebService {
+    @Override
+    public String getHelloString(String name) {
+        // просто возвращаем приветствие
+        return "Hello, " + name + "!";
+    }
+}
