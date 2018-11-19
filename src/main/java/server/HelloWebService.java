6dc5d5eff3203b586843988f2d27d08ca2e1b566
Ekker_Artem <40545726+jetess@users.noreply.github.com>
2018-11-19 13:07:09 +0200

    Server part of web service refactored to count calories, proteins, fats and carbohydrates.

--- src/main/java/server/HelloWebService.java
+++ src/main/java/server/HelloWebService.java
@@ -5,24 +5,30 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import java.util.Date;
 
-// говорим, что наш интерфейс будет работать как веб-сервис
 @WebService
-// говорим, что веб-сервис будет использоваться для вызова методов
+
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface HelloWebService {
-    // говорим, что этот метод можно вызывать удаленно
+
     @WebMethod
     public String getHelloString(String name);
-
-    @WebMethod
-    public double radnomizer();
-
-    @WebMethod
-    public String getRandomProgrammingLanguage();
 
     @WebMethod
     public Date getCurrentTime();
 
     @WebMethod
     public String getByeString(String name);
+
+    public String getCalories(String meal, int count);
+
+    public String getFats(String meal, int count);
+
+    public String getProteins(String meal, int count);
+
+    public String getHarbohydrates(String meal, int count);
+
+
+
+
+
 }
--- src/main/java/server/impl/HelloWebServiceImpl.java
+++ src/main/java/server/impl/HelloWebServiceImpl.java
@@ -1,42 +1,16 @@
 package server.impl;
 
-// таже аннотация, что и при описании интерфейса,
 import server.HelloWebService;
 
 import javax.jws.WebService;
 import java.util.Date;
 
-// но здесь используется с параметром endpointInterface,
-// указывающим полное имя класса интерфейса нашего веб-сервиса
 @WebService(endpointInterface = "server.HelloWebService")
 public class HelloWebServiceImpl implements HelloWebService {
+
     @Override
     public String getHelloString(String name) {
-        // просто возвращаем приветствие
         return "Hello, " + name + "!";
-    }
-
-    @Override
-    public double radnomizer() {
-        double rez = Math.random()*100;
-        return rez;
-    }
-
-    @Override
-    public String getRandomProgrammingLanguage() {
-        double rez = Math.random()*100;
-        String rezString = null;
-        if (rez>=0 && rez <20)
-            rezString = "Java";
-        else if (rez>=20 && rez <40)
-            rezString = "C++";
-        else if (rez>=40 && rez <60)
-            rezString = "C#";
-        else if (rez>=60 && rez <80)
-            rezString = "Objective C";
-        else if (rez>=80 && rez <=100)
-            rezString = "JavaScript";
-        return rezString;
     }
 
     @Override
@@ -49,6 +23,61 @@ public class HelloWebServiceImpl implements HelloWebService {
         return "Bye, " + name + ":)";
     }
 
+    @Override
+    public String getCalories(String meal, int count) {
+        double rez;
+        if (meal.toLowerCase().equals("мясо")) {
+            rez = count * 0.5;
+        } else if (meal.toLowerCase().equals("рыба")) {
+            rez = count * 1.5;
+        } else if (meal.toLowerCase().equals("курица")) {
+            rez = count * 1.0;
+        } else rez = 0;
+        return "Вы съели :" + meal + " (" + count + " грамм)" + " и получили " + rez + " калорий!";
+    }
+
+    @Override
+    public String getFats(String meal, int count) {
+        double rez;
+        if (meal.toLowerCase().equals("мясо")) {
+            rez = count * 0.3;
+        } else if (meal.toLowerCase().equals("рыба")) {
+            rez = count * 0.1;
+        } else if (meal.toLowerCase().equals("курица")) {
+            rez = count * 0.2;
+        } else rez = 0;
+        return "Вы съели :" + meal + " (" + count + " грамм)" + " и получили " + rez + " жиров!";
+    }
+
+    @Override
+    public String getProteins(String meal, int count) {
+        double rez;
+        if (meal.toLowerCase().equals("мясо")) {
+            rez = count * 0.5;
+        } else if (meal.toLowerCase().equals("рыба")) {
+            rez = count * 0.4;
+        } else if (meal.toLowerCase().equals("курица")) {
+            rez = count * 0.7;
+        } else rez = 0;
+        return "Вы съели :" + meal + " (" + count + " грамм)" + " и получили " + rez + " белка!";
+    }
+
+    @Override
+    public String getHarbohydrates(String meal, int count) {
+        double rez;
+        if (meal.toLowerCase().equals("мясо")) {
+            rez = count * 0.2;
+        } else if (meal.toLowerCase().equals("рыба")) {
+            rez = count * 0.05;
+        } else if (meal.toLowerCase().equals("курица")) {
+            rez = count * 0.1;
+        } else rez = 0;
+        return "Вы съели :" + meal + " (" + count + " грамм)" + " и получили " + rez + " углеводов!";
+    }
+
+
 }
+
+
 
 
