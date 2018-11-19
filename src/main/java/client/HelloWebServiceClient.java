09c0120f39b7bc5f833e809e7fb9cf3e0368149b
Ekker_Artem <40545726+jetess@users.noreply.github.com>
2018-11-19 13:07:49 +0200

    Client part of web service refactored to support new web service part.

--- src/main/java/client/HelloWebServiceClient.java
+++ src/main/java/client/HelloWebServiceClient.java
@@ -2,6 +2,7 @@ package client;
 
 // нужно, чтобы получить wsdl описание и через него
 // дотянуться до самого веб-сервиса
+import java.io.FileNotFoundException;
 import java.net.URL;
 // такой эксепшн возникнет при работе с объектом URL
 import java.net.MalformedURLException;
@@ -9,13 +10,14 @@ import java.net.MalformedURLException;
 // классы, чтобы пропарсить xml-ку c wsdl описанием
 // и дотянуться до тега service в нем
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.ws.Service;
 
 // интерфейс нашего веб-сервиса (нам больше и нужно)
 import server.HelloWebService;
 
 public class HelloWebServiceClient {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, FileNotFoundException, XMLStreamException {
         // создаем ссылку на wsdl описание
         URL url = new URL("http://localhost:1986/wss/hello?wsdl");
 
@@ -34,9 +36,19 @@ public class HelloWebServiceClient {
 
         // Ура! Теперь можно вызывать удаленный метод
         System.out.println(hello.getHelloString("Artem Ekker and Alex Konovalov"));
-        System.out.println(hello.radnomizer());
-        System.out.println(hello.getRandomProgrammingLanguage());
-        System.out.println(hello.getCurrentTime());
+        System.out.print("Current time and date : -----> ");
+        System.out.print(hello.getCurrentTime());
+        System.out.println(" <-----");
+        System.out.println(" ");
+        System.out.println(hello.getCalories("мясо",150));
+        System.out.println(hello.getProteins("мясо",150));
+        System.out.println(hello.getFats("мясо",150));
+        System.out.println(hello.getHarbohydrates("мясо",150));
+        System.out.println(hello.getCalories("Рыба",250));
+        System.out.println(hello.getProteins("Рыба",250));
+        System.out.println(hello.getFats("Рыба",250));
+        System.out.println(hello.getHarbohydrates("Рыба",250));
+        System.out.println(" ");
         System.out.println(hello.getByeString("Artem Ekker and Alex Konovalov"));
     }
 }
